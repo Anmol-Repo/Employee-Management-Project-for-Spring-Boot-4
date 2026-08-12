9af108f11ca181d10d32b5831893b82851c921df
Anmol-Repo <148074536+Anmol-Repo@users.noreply.github.com>
2026-08-12 07:05:10 +0530

    Configure Routing

--- ems-frontend/src/App.tsx
+++ ems-frontend/src/App.tsx
@@ -2,17 +2,22 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-
   return (
-  <>
-  <HeaderComponent/>
- <ListEmployeeComponent/>
- <FooterComponent/>
+    <BrowserRouter>
+      <HeaderComponent />
+      
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />} />
+          <Route path="/employees" element={<ListEmployeeComponent />} />
+        </Routes>
+      </main>
 
-
-  </>
+      <FooterComponent />
+    </BrowserRouter>
   )
 }
 
--- ems-frontend/src/components/FooterComponent.tsx
+++ ems-frontend/src/components/FooterComponent.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div>
-        <footer className="footer">
-            <span>All rights reserved by Anmol </span>
-        </footer>
-    </div>
+    <footer className="footer">
+      <span>All rights reserved by Anmol</span>
+    </footer>
   )
 }
 
